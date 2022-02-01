eeef8f7a3677e33782b94a722330699925da23ad
Jayanth <jayanth151002@gmail.com>
2022-02-02 00:04:37 +0530

    Initial Pieces arragnement

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -5,24 +5,56 @@ import Chess from 'chess.js'
 
 const Board = () => {
 
-    const [pieces, setPieces] = useState({ K: "", k: "", Q: "", q: "", R: "", r: "", B: "", b: "", N: "", n: "", P: "", p: "" })
+    const [pieces, setPieces] = useState({ "K": "", "k": "", "Q": "", "q": "", "R": "", "r": "", "B": "", "b": "", "N": "", "n": "", "P": "", "p": "" })
     const [game, setGame] = useState(new Chess())
+    const [fen, setFen] = useState(game.fen())
 
+    const [piecepos, setpiecepos] = useState({ K: "e1", Q: "d1", R1: "a1", R2: "h1", B1: "c1", B2: "f1", N1: "b1", N2: "g1", P1: "a2", P2: "b2", P3: "c2", P4: "d2", P5: "e2", P6: "f2", P7: "g2", P8: "h2", k: "e8", q: "d8", r1: "a8", r2: "h8", b1: "c8", b2: "f8", n1: "b8", n2: "g8", p1: "a7", p2: "b7", p3: "c7", p4: "d7", p5: "e7", p6: "f7", p7: "g7", p8: "h7" })
+
+    var numarr = [8, 7, 6, 5, 4, 3, 2, 1]
+    var alpharr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+    const positions = numarr.map((n) => {
+        const rowpos = alpharr.map((a) => {
+            if (Object.values(piecepos).includes(a + n)) {
+                var key = Object.keys(piecepos).find(k => piecepos[k] === a + n)
+                return (
+                    <div className="board-square">
+                        <img src={pieces[key[0]]} alt="" width="100%" />
+                    </div>
+                )
+            } else return (
+                <div className="board-square">
+
+                </div>
+            )
+        })
+        //  return rowpos
+        return (
+            <div className="col-12">
+                <div className={n % 2 == 0 ? "board-row-0" : "board-row-1"}>
+                    {rowpos}
+                </div>
+            </div>
+        )
+    })
 
     axios.get('/pieces')
         .then(res => setPieces(res.data))
         .catch(res => console.log(res))
-    console.log(pieces)
+
     return (
-        <div className='container'>
+        <div className='container' >
+
             <div className="row">
-                <div className="col-12">
+                {positions}
+                {/* <div className="col-12">
                     <div className="board-row-0">
                         <div className="board-square">
                             1
                         </div>
                         <div className="board-square">
-                            <img src={pieces.k} alt="" />
+                            <img src={pieces["k"]} alt="" />
                         </div>
                         <div className="board-square">
                             1
@@ -37,7 +69,7 @@ const Board = () => {
                             1
                         </div>
                         <div className="board-square">
-                        <img src={pieces.Q} alt="" />
+                            <img src={pieces["Q"]} alt="" />
                         </div>
                         <div className="board-square">
                             1
@@ -81,7 +113,7 @@ const Board = () => {
                             1
                         </div>
                         <div className="board-square">
-                        <img src={pieces.n} alt="" />
+                            <img src={pieces["n"]} alt="" />
                         </div>
                         <div className="board-square">
                             1
@@ -140,7 +172,7 @@ const Board = () => {
                             1
                         </div>
                         <div className="board-square">
-                        <img src={pieces.p} alt="" />
+                            <img src={pieces["p"]} alt="" />
                         </div>
                         <div className="board-square">
                             1
@@ -165,7 +197,7 @@ const Board = () => {
                             1
                         </div>
                         <div className="board-square">
-                        <img src={pieces.r} alt="" />
+                            <img src={pieces["r"]} alt="" />
                         </div>
                         <div className="board-square">
                             1
@@ -230,7 +262,7 @@ const Board = () => {
                             1
                         </div>
                         <div className="board-square">
-                        <img src={pieces.K} alt="" />
+                            <img src={pieces["K"]} alt="" />
                         </div>
                         <div className="board-square">
                             1
@@ -239,9 +271,9 @@ const Board = () => {
                             1
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
-        </div>
+        </div >
     )
 }
 
--- server/assets/pieces.js
+++ server/assets/pieces.js
@@ -1,17 +1,17 @@
 const pieces =
 {
-    K: "https://res.cloudinary.com/jay-1510/image/upload/v1643723556/Pieces/wk_y4jxe6.png",
-    k: "https://res.cloudinary.com/jay-1510/image/upload/v1643723555/Pieces/bk_ekjrgq.png",
-    Q: "https://res.cloudinary.com/jay-1510/image/upload/v1643723556/Pieces/wq_tclwut.png",
-    q: "https://res.cloudinary.com/jay-1510/image/upload/v1643723555/Pieces/bq_smyepa.png",
-    R: "https://res.cloudinary.com/jay-1510/image/upload/v1643723556/Pieces/wr_pe7b5p.png",
-    r: "https://res.cloudinary.com/jay-1510/image/upload/v1643723555/Pieces/br_nbo4ut.png",
-    B: "https://res.cloudinary.com/jay-1510/image/upload/v1643723556/Pieces/wb_rpgiz4.png",
-    b: "https://res.cloudinary.com/jay-1510/image/upload/v1643723555/Pieces/bb_fk6fke.png",
-    N: "https://res.cloudinary.com/jay-1510/image/upload/v1643723556/Pieces/wn_hp2ssy.png",
-    n: "https://res.cloudinary.com/jay-1510/image/upload/v1643723555/Pieces/bn_we8jda.png",
-    P: "https://res.cloudinary.com/jay-1510/image/upload/v1643723556/Pieces/wp_vd46th.png",
-    p: "https://res.cloudinary.com/jay-1510/image/upload/v1643723555/Pieces/bp_ckyoar.png"
+    "K": "https://res.cloudinary.com/jay-1510/image/upload/v1643723556/Pieces/wk_y4jxe6.png",
+    "k": "https://res.cloudinary.com/jay-1510/image/upload/v1643723555/Pieces/bk_ekjrgq.png",
+    "Q": "https://res.cloudinary.com/jay-1510/image/upload/v1643723556/Pieces/wq_tclwut.png",
+    "q": "https://res.cloudinary.com/jay-1510/image/upload/v1643723555/Pieces/bq_smyepa.png",
+    "R": "https://res.cloudinary.com/jay-1510/image/upload/v1643723556/Pieces/wr_pe7b5p.png",
+    "r": "https://res.cloudinary.com/jay-1510/image/upload/v1643723555/Pieces/br_nbo4ut.png",
+    "B": "https://res.cloudinary.com/jay-1510/image/upload/v1643723556/Pieces/wb_rpgiz4.png",
+    "b": "https://res.cloudinary.com/jay-1510/image/upload/v1643723555/Pieces/bb_fk6fke.png",
+    "N": "https://res.cloudinary.com/jay-1510/image/upload/v1643723556/Pieces/wn_hp2ssy.png",
+    "n": "https://res.cloudinary.com/jay-1510/image/upload/v1643723555/Pieces/bn_we8jda.png",
+    "P": "https://res.cloudinary.com/jay-1510/image/upload/v1643723556/Pieces/wp_vd46th.png",
+    "p": "https://res.cloudinary.com/jay-1510/image/upload/v1643723555/Pieces/bp_ckyoar.png"
 }
 
 module.exports = pieces
